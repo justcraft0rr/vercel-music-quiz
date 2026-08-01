f079c2fab8f8cb13d870c61197dd80f2cac57ca2
justcraft0rr <justcraftytgm@gmail.com>
2026-08-01 14:42:34 +0200

    image update

--- commit.sh
+++ commit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
+echo 'What is the commit gonna be named, sir?'
+read name
 git init
 git add .
-git commit -m "Initial commit"
+git commit -m "$name"
 git remote add origin https://github.com/yourname/my-project.git
 git push -u origin main
 echo 'Commit Finished'
